4783ee72a8580fbc2d591f794981f1ca2f233c90
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2024-04-05 21:35:49 +0300

    Py-function prints all integers of a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,5 @@ def print_list_integer(my_list=[]):
     i = 0
     a = len(my_list)
     while i < a:
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
         i += 1
