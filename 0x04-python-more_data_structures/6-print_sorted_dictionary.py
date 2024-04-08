26b9160aa1a3294db67f1777f683cc807cad7831
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2024-04-08 21:18:59 +0300

    prints a dictionary by ordered keys

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# printing a dictionary by ordered keys(all keys are assumed to be 
+# strings sorted in alphabetical order) only keys at first level are
+# sorted, keys of a dictionary inside the main dictionary are not
+# sorted. Dictionary values can have any type.
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary.keys()):
+        value = a_dictionary[key]
+        if isinstance(value, dict):
+            print_sorted_dictionary(value)
+        else:
+            print("{}: {}".format(key, value))
