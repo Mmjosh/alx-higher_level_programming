109663b8cf8ee17130d3dea9fe622a4fe0dc18f9
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2024-06-28 21:59:30 +0300

    Update `Square`: added method `area`

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""defining a class: `Square`"""
+
+
+class Square:
+    """
+    a class `Square` with a private attribute `size` and
+    checks data type
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """calculates area of the square"""
+        return self.__size ** 2
