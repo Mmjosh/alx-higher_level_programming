498d783e6bcb4dfa5f898b571735ffddd2d4515b
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2024-07-24 18:46:58 +0300

    adds more depth to the class `Rectangle`

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class `Rectangle` that defines
+a rectangle
+"""
+
+
+class Rectangle:
+    """
+    A class used to represent a rectangle
+
+    Attributes:
+        __width (int): width of the rectangle
+        __height (int): height of the rectangle
+
+    Methods:
+        __init__(self, width, height): initializes the rectangle
+        width(self): gets the width of the rectangle
+        width(self, value): sets the width
+        height(self): gets the height of the rectangle
+        height(self, value): sets the height of the rectangle
+    """
+    def __init__(self, width, height):
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
